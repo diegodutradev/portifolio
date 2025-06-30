493b72c61cb5be39798febd20dfb2d605786d9e6
Diego Dutra <diego.dutradev@gmail.com>
2025-06-30 00:28:02 -0300

    atualizando o vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// vite.config.ts ou vite.config.js
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
 })
+
